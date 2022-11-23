3178adb6b247072b135ed72c90db1255714b649c
Dzmitry_Karashkou <Dzmitry_Karashkou@epam.com>
2022-11-23 09:50:37 +0300

    add unit tests

--- src/main/java/com/koreshkov/tinkoffservice/service/TinkoffStockService.java
+++ src/main/java/com/koreshkov/tinkoffservice/service/TinkoffStockService.java
@@ -26,7 +26,7 @@ import java.util.stream.Collectors;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class TinkoffStockSeervice implements StockService{
+public class TinkoffStockService implements StockService{
     private final OpenApi openApi;
 
     @Async
